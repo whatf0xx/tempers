c95c9bec3c8d36469aa6b380f48d5c8f258d97e9
whatf0xx <whatf0xx@gmail.com>
2024-01-01 11:53:39 +0100

    Delete generate_tests.py

--- generate_tests.py
+++
@@ -1,8 +0,0 @@
-from random import Random
-
-seed: int = 5489
-r = Random(x=seed)
-
-with open("init_default_state.txt", "w", encoding="utf-8") as f:
-    for num in r.getstate()[1]:
-        f.write(f"{num}\n")
