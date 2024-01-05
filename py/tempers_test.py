6e2ac0f3dd99aab5e3e7e01ef4553a6b41ce1640
whatf0xx <hfox022@gmail.com>
2024-01-05 23:19:46 +0100

    Working pytest, make sure you activate virtual environment (python 3.11)

--- py/tempers_test.py
+++ py/tempers_test.py
@@ -1,9 +1,8 @@
-from random import Random
 import ctypes
 
-if __name__ == "__main__":
+def test_load_mt():
     my_lib = ctypes.CDLL("./target/debug/libtempers.so")
     my_rand = my_lib.seeded_generator_ptr(5489)
     print(f"{ctypes.c_uint(my_rand).value:x}")
     generated_value = ctypes.c_uint32(my_lib.generate_random_u32(my_rand)).value
-    print(generated_value)
+    assert  generated_value == 3499211612
--- src/mt19937_ffi.rs
+++ src/mt19937_ffi.rs
@@ -2,10 +2,14 @@ use crate::mt19937::MT19937;
 
 #[no_mangle]
 pub extern "C" fn seeded_generator_ptr(seed: u32) -> *mut MT19937 {
-    Box::into_raw(Box::new(MT19937::from_seed(seed)))
+    let boxed_mt = Box::new(MT19937::from_seed(seed));
+    let raw_ptr = Box::into_raw(boxed_mt);
+    println!("MT allocated at {:?}", raw_ptr);
+    raw_ptr
 }
 
 #[no_mangle]
 pub extern "C" fn generate_random_u32(generator: &mut MT19937) -> u32 {
+    println!("Received a pointer to {:p}", generator);
     generator.next().unwrap()
 }
